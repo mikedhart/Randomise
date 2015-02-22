f041ee01a7960af0fcfc225b00fefd6320213c9a
Mike <mikedhart@gmail.com>
2015-02-22 16:29:15 +0000

    ADD: Date

--- dist/jquery.randomiser.js
+++ dist/jquery.randomiser.js
@@ -2,20 +2,41 @@ $.fn.randomise = function() {
 	this.each(function(i, el) {
 		switch ($(el).attr('type')) {
     	case 'number':
-				$(el).val(random_number());
+				$(el).attr('value', random_number());
+				break;
+			case 'date':
+				$(el).attr('value', random_date());
 				break;
 			default:
-				$(el).val(random_string());
+				$(el).attr('value', random_string());
 				break;
 		}
 	});
 
-	function random_string() {
-		return Math.random().toString(36).substring(7); 
+	function random_string(n) {
+		if (!n) {
+			n = 10;
+		}
+
+		var text = '';
+		var possible = 'abcdefghijklmnopqrstuvwxyz';
+
+		for (var i = 0; i < n; i++) {
+			text += possible.charAt(Math.floor(Math.random() * possible.length));
+		}
+
+		return text;
 	}
 
 	function random_number() {
-		return Math.random(); 
+		return Math.floor(Math.random() * 1000); 
+	}
+
+	function random_date() {
+		now = new Date();
+		date = new Date(now.getTime() + Math.random() * Math.random());
+		console.log(date);
+		return date;
 	}
 };
 
